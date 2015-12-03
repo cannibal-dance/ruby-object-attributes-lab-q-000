3fb248de08e9320ed15c4cf40866d6c62892afdf
Jesse <homestarflyer@gmail.com>
2015-12-03 22:29:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(name_str)
+    @name = name_str
+  end
+  def name
+    @name
+  end
+  def breed=(breed_str)
+    @breed = breed_str
+  end
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(name_str)
+    @name = name_str
+  end
+  def name
+    @name
+  end
+  def job=(job_str)
+    @job = job_str
+  end
+  def job
+    @job
+  end
+end
